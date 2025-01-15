28c17c40550550e1f38ef11f0c4d0b208674654b
Akshansh0073 <sakshansh.bv@gmail.com>
2025-01-15 09:11:36 +0530

    first commit

---
+++ src/main/java/com/lcwd/multiple/db/spring_boot_multipledb/SpringBootMultipledbApplication.java
@@ -0,0 +1,13 @@
+package com.lcwd.multiple.db.spring_boot_multipledb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMultipledbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMultipledbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lcwd/multiple/db/spring_boot_multipledb/SpringBootMultipledbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lcwd.multiple.db.spring_boot_multipledb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMultipledbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
